b6e49c3250838a7c2d5c556b48d0e473c48be9db
cesarmosqueira <cesarmanuel.2802@gmail.com>
2021-05-18 02:27:34 -0500

    Chunks working     - Procedurally generated Missing     - Pre-defined shadows     - Lighting     - Efficient rendering     - Group a whole mesh onto a single draw call

--- include/Chunk.hpp
+++ include/Chunk.hpp
@@ -1,16 +1,18 @@
 #include "Block.hpp"
 #include "ZNOISE_INCLUDE/Perlin.hpp"
 #include "ZNOISE_INCLUDE/FBM.hpp"
-#define CHUNK_HEIGHT 256
+#define CHUNK_HEIGHT 128
 #define CHUNK_SIDE 16
 float h;
 bool vis;
+
 class Chunk { 
 public: 
-    Chunk(const ui32& _x, const ui32& _z, FBM*& noise) : XPOS(_x), ZPOS(_z) { 
-        blocks = new Block**[CHUNK_HEIGHT];
+    Chunk(const ui32& _x, const ui32& _z, FBM*& noise, const f32& max_h) : XPOS(_x), ZPOS(_z) { 
         XOFF = XPOS * CHUNK_SIDE;
         ZOFF = ZPOS * CHUNK_SIDE;
+
+        blocks = new Block**[CHUNK_HEIGHT];
         for(ui32 y = 0; y < CHUNK_HEIGHT; y++) { 
 
             blocks[y] = new Block*[CHUNK_SIDE];
@@ -19,9 +21,9 @@ public:
                 blocks[y][x] = new Block[CHUNK_SIDE];
 
                 for(ui32 z = 0; z < CHUNK_SIDE; z++){
-
-                    h = noise->Get({(float)x,(float)z}, 0.01f) * (float)CHUNK_HEIGHT/2;
-                    vis = y < h;
+            
+                    h = noise->Get({(float)(x+XOFF),(float)(z+ZOFF)}, 0.007f) * max_h;
+                    vis = y && y < h;
                     blocks[y][x][z].init(y, x + XOFF, z + ZOFF, vis);
 
                 }
@@ -31,8 +33,14 @@ public:
     ~Chunk() { 
         delete_blocks();
     }
-    Block***& Data() {
+    Block*** Data() {
         return blocks;
+    }
+    int X() { 
+        return XPOS;
+    }
+    int Z() { 
+        return ZPOS;
     }
 private:
     void delete_blocks() {
@@ -44,7 +52,7 @@ private:
         }
         delete[] blocks;
     }
-    
+     
 private:
     ui32 XPOS, ZPOS;
     Block ***blocks;
--- include/pncraft.hpp
+++ include/pncraft.hpp
@@ -1,6 +1,19 @@
 #include "Konstants.hpp"
 #include "IndexBuffer.hpp"
 #include "Chunk.hpp"
+#include <vector>
+
+
+Chunk* find_chunk(const std::vector<Chunk*>& vec, 
+        const i32& x, const i32& z){
+    for(Chunk* c : vec){
+        if (c->X() == x && c->Z() == z){
+            return c;
+        }
+    }
+    return nullptr;
+}
+
 
 class World {
 public:
@@ -11,7 +24,6 @@ private:
     ui32 textureID; 
     i32 SCR_WIDTH, SCR_HEIGHT;
     ui32 VAO, VBO; 
-    Chunk*** chunks;
     /*TODO: Make this IBO's set fancier*/
     IndexBuffer* UP;
     IndexBuffer* NORTH;
@@ -19,26 +31,31 @@ private:
     IndexBuffer* WEST;
     IndexBuffer* SOUTH;
     IndexBuffer* DOWN;
+    std::vector<Chunk*> chunks;
+    std::vector<Chunk*> MAP;
 
     Perlin perlin;
     FBM* noise;
     glm::mat4 projection;
-    ui32 chunks_count;
+    Chunk* current_chunk, *chunk_aux;
+    f32 max_h;
+    i32 chunking;
+    i32 xChunk, zChunk;
 public:
     World(const int& code) : 
         program(new Shader("shaders/coord/")), projection(glm::mat4(1.0f)) {
-        chunks_count = 4; 
-        chunks = new Chunk**[chunks_count];
-        for(ui32 i = 0; i < chunks_count; i++) {
-            chunks[i] = new Chunk*[chunks_count];
-            for(ui32 j = 0; j < chunks_count; j++) { 
-                chunks[i][j] = new Chunk(i,j, noise);
-            }
-        }
+        noise = new FBM(perlin);
+        xChunk = 0;
+        zChunk = 0;
 
+        chunking = 6;
+        max_h = CHUNK_HEIGHT - CHUNK_HEIGHT/4.0f;
+        chunk_update();
+        //init_first_chunks();
+        generate_chunks();
+        
         view_distance = 48;
         this->wireframe = false;
-        noise = new FBM(perlin);
         this->mem_init();
         switch(code){
         case 1: 
@@ -55,17 +72,12 @@ public:
     }
     ~World(){
         /* clean object */
-        
-
-        for(ui32 i = 0; i < chunks_count; i++) {
-            for(ui32 j = 0; j < chunks_count; j++) { 
-                delete chunks[i][j];
-            }
-            delete[] chunks[i];
+        for(int i = 0; i < chunks.size(); i++) {
+            if(chunks[i]) delete chunks[i];
         }
-        delete[] chunks;
-        std::cout << "Chunks deleted succesfully\n";
-
+        for(int i = 0; i < MAP.size(); i++) {
+            if(MAP[i]) delete MAP[i];
+        }
         if(UP) delete UP;
         if(NORTH) delete NORTH;
         if(EAST) delete EAST;
@@ -75,6 +87,7 @@ public:
         if(DOWN) delete DOWN;
         if(program) delete program;
         if(noise) delete noise;
+        if(chunk_aux) delete chunk_aux;
         std::cout << "World deleted succesfully\n";
     }
     void update_width_height(const int& w, const int& h) {
@@ -96,97 +109,97 @@ public:
     void on_update(const glm::vec3& pp) {
         /* Enable shader */
         program->useProgram();
+        if (int(pp[0]/CHUNK_SIDE) != xChunk || int(pp[2]/CHUNK_SIDE) != zChunk) {
+            //update xChunk and zChunk
+            xChunk =int(pp[0]/CHUNK_SIDE);
+            zChunk =int(pp[2]/CHUNK_SIDE);
+            chunk_update();
+            generate_chunks();
+        }
         /* commpute matrices to send as uniforms */
         glPolygonMode( GL_FRONT_AND_BACK, wireframe ? GL_LINE : GL_FILL);
-
-
-        i32 sx = K::max<i32>(pp[0] - view_distance, 0);
-        sx = K::min<i32>(sx, CHUNK_SIDE);
-
-        i32 lx = K::min<i32>(pp[0] + view_distance, CHUNK_SIDE);
-        lx = K::max<i32>(lx, 0);
-
-        i32 sz = K::max<i32>(pp[2] - view_distance, 0);
-        sz = K::min<i32>(sz, CHUNK_SIDE);
-
-        i32 lz = K::min<i32>(pp[2] + view_distance, CHUNK_SIDE);
-        lz = K::max<i32>(lz, 0);
-
-
-
-        
-        for(int i = 0; i < 4; i++ ) {
-            for(int j = 0; j < 4; j++ ) {
-                chunk_draw_call(chunks[i][j]->Data());
-            }
-        }
+        for(Chunk* c : chunks)
+            chunk_draw_call(c->Data());
         /* Send block matrices to shaders */
     }
 
     void chunk_draw_call(Block*** data) { 
-        for(ui32 y = 0; y < CHUNK_HEIGHT; y++){
+        for(i32 y =0 ; y < CHUNK_HEIGHT; y++){
 
-            for(ui32 x = 0; x < CHUNK_SIDE; x++){
+            for(i32 x = 0; x < CHUNK_SIDE; x++){
 
-                for(ui32 z = 0; z < CHUNK_SIDE; z++){
+                for(i32 z = 0; z < CHUNK_SIDE; z++){
                     block_draw_call(data, x,y,z);
                 }
             }
         }
-
-
     }
 
-    void block_draw_call(Block***& data, const i32& x, const i32&y, const i32& z) {
+    void block_draw_call(Block*** data, const i32& x, const i32&y, const i32& z) {
         if ( data[y][x][z].is_solid() ) {
-            program->setMat4("model", data[y][x][z].model);
-            face_draw_call(data,    UP, y,x,z,  'U');
-            face_draw_call(data, NORTH, y,x,z,  'N');
-            face_draw_call(data,  EAST, y,x,z,  'E');
-            face_draw_call(data,  WEST, y,x,z,  'W');
-            face_draw_call(data, SOUTH, y,x,z,  'S');
-            face_draw_call(data,  DOWN, y,x,z,  'D');
+            face_draw_call(data,    UP, x,y,z , 'U');
+            face_draw_call(data, NORTH, x,y,z , 'N');
+            face_draw_call(data,  EAST, x,y,z , 'E');
+            face_draw_call(data,  WEST, x,y,z , 'W');
+            face_draw_call(data, SOUTH, x,y,z , 'S');
+            if(y > 1) face_draw_call(data,  DOWN, x,y,z , 'D');
         }
     }
     void face_draw_call(Block***& data,const IndexBuffer* face, 
            const i32& x, const i32&y, const i32& z, const ui8& code) { 
+
         if(!not_visible(data, x,y,z, code)){
+            program->setMat4("model", data[y][x][z].model);
             face->bind();
             glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
         } 
     }
-    bool not_visible(Block***& blocks, const ui32& x, const ui32& y, const ui32& z, const ui32& dir){ 
-        if(dir == 'D') {
+    bool not_visible(Block***& blocks, const ui32& x, const ui32& y, const ui32& z, 
+            const ui32& dir){ 
+        if(dir == 'D') { //if in the bottom of the chunk
             if ( 0 < y ) {
                 return blocks[y-1][x][z].is_solid();
             }
             else return false;
         }
+        /* MUST SUPPORT BLOCKS FACING OTHER CHUNKS */
         if(dir == 'N') { 
+            /* - Validate if its on the border of the current chunk - */
             if ( x < CHUNK_SIDE - 1 ) {
                 return blocks[y][x + 1][z].is_solid();
             }
-            else return false;
+            else { //its on the border
+                //validate with the adjacent chunk to (x+1)  
+                 
+                return false;
+            }
         }
         if(dir == 'W') { 
             if ( z < CHUNK_SIDE - 1 ) {
                 return blocks[y][x][z+1].is_solid();
             }
-            else return false;
+            else {
+                return false;
+            }
         }
         if(dir == 'E') { 
             if ( 0 < z ) {
                 return blocks[y][x][z-1].is_solid();
             }
-            return false;
+            else { 
+                return false;
+            }
         }
         if(dir == 'S') { 
             if ( 0 < x ) {
                 return blocks[y][x-1][z].is_solid();
             }
-            else return false;
+            else {
+                return false;
+            }
         }
-        if(dir == 'U'){
+        /* --------------------------------------- */
+        if(dir == 'U'){ //if its on top of the chunk
             if ( y < CHUNK_HEIGHT-1 ) { 
                 return blocks[y+1][x][z].is_solid();
             }
@@ -196,9 +209,34 @@ public:
         return false;
     }
     Shader* s() { return program; }
-    
-    void mem_init(){
 
+    void chunk_update(){
+        //search for chunk in MAP cache
+        chunks.clear();
+        Chunk* act = find_chunk(MAP, xChunk, zChunk);
+        //if current chunk is in cache already, current is replaced
+        if(!act) {
+            act = new Chunk(xChunk, zChunk, noise, max_h);
+        }
+        current_chunk = act;
+        MAP.push_back(act); //map will store all the chunks ever computed
+    }
+    ///
+    /// Generates adjacents chunks to the actual chunk where the player is standing.
+    ///
+    void generate_chunks() {
+        for(i32 i = current_chunk->X() - chunking; i <= current_chunk->X() + chunking; i++){
+            for(i32 j = current_chunk->Z() - chunking; j <= current_chunk->Z() + chunking; j++){
+                chunk_aux = find_chunk(MAP, i, j);
+                if(!chunk_aux) {
+                    chunk_aux = new Chunk(i,j, noise, max_h);
+                    MAP.push_back(chunk_aux);
+                }
+                chunks.push_back(chunk_aux);
+            }
+        }
+    }
+    void mem_init(){
         glGenVertexArrays(1, &VAO);
         glBindVertexArray(VAO);
 
@@ -217,7 +255,7 @@ public:
         // posiciones
         glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8*sizeof(float), (void*)0);
         glEnableVertexAttribArray(0);
-        // colores
+        // shadows
         glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8*sizeof(float), (void*)(3*sizeof(float)));
         glEnableVertexAttribArray(1);
         // coordenadas de textura
@@ -225,8 +263,5 @@ public:
         glEnableVertexAttribArray(2);
 
         glBindVertexArray(0);
-        
     }
 };
-    
-    
